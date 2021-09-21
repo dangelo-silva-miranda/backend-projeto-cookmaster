7e57a1424e699b15bbd9193ab416d6ff147e4fef
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-21 18:55:24 -0300

    feat: :sparkles: adds userRouter with address '/users' in app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const { userRouter } = require('../routers/userRouter');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,5 +10,8 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+/* Todas as rotas com /users/<alguma-coisa> entram aqui e vão para o roteador */
+app.use('/users', userRouter);
 
 module.exports = app;
