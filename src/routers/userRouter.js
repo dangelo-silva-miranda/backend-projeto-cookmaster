5cd5e8c86a06c2a01e2704ad0eb15585c53e608e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-21 19:59:19 -0300

    feat: :sparkles: implements validations in userRouter.post with address '/'

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const { validateName, validateEmail, validatePassword } = require('../middlewares/validations');
 
 const userRouter = express.Router();
 
-userRouter.post('/', userController.createUser);
+userRouter.post('/', [validateName, validateEmail, validatePassword], userController.createUser);
 
 module.exports = { userRouter };
