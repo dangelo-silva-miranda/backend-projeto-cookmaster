162a16d17a8d5ada39df611a2c5bfab29afa30b4
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-21 19:57:18 -0300

    feat: :sparkles: implements userEmailExists function in userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,11 +2,19 @@
 
 const connection = require('./connection');
 
+const userEmailExists = async (email) => {
+  const user = await connection.getConnection()
+  .then((db) => db.collection('users').findOne({ email }));
+
+  return user !== null;
+};
+
 const createUser = async ({ name, email, password, role }) => connection.getConnection()
 .then((db) => db.collection('users')
 .insertOne({ name, email, password, role }))
 .then((result) => result.ops[0]);
 
 module.exports = {
+  userEmailExists,
   createUser,
 };
