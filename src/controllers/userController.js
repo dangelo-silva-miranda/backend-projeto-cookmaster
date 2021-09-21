d253071da04dbae6ae818e9231964d6e795fe3a6
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-21 18:48:38 -0300

    feat: :sparkles: implements createUser in userController

---
+++ src/controllers/userController.js
@@ -0,0 +1,23 @@
+const userService = require('../services/userService');
+
+/*
+  Material consultado sobre req.path
+  http://expressjs.com/pt-br/api.html#req.path
+
+*/
+const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const { path } = req;
+  
+  const role = (path.includes('admin')) ? 'admin' : 'user';
+  
+  const { status, message } = await userService.createUser(
+     { name, email, password, role },
+   );
+
+  return res.status(status).json(message);
+};
+
+module.exports = {
+  createUser,
+};
