411287f608fbe0cc27fc8db0c43326576173ef4f
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-23 07:45:39 -0300

    feat: :sparkles: implements createRecipe in recipeService

---
+++ src/services/recipeService.js
@@ -0,0 +1,18 @@
+const { StatusCodes } = require('http-status-codes');
+const recipeModel = require('../models/recipeModel');
+
+const createRecipe = async ({ name, ingredients, preparation, 
+  userId, image }) => {
+  const recipe = await recipeModel.createRecipe(
+    { name, ingredients, preparation, userId, image },
+  );
+
+  return {
+    status: StatusCodes.CREATED,
+    message: { recipe },
+  };
+};
+
+module.exports = {
+  createRecipe,
+};
