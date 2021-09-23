07356b6bc8bb64e37c5bc7af8077579aeeab2b11
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-23 06:55:50 -0300

    feat: :sparkles: implements validateLoginEmail and validateLoginPassword functions

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -44,8 +44,48 @@ const validatePassword = (req, res, next) => {
   next();
 };
 
+const validateLoginEmail = async (req, res, next) => {
+  const { email = '' } = req.body;
+
+  if (email === '') {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'All fields must be filled',
+    });
+  }
+
+  const emailPattern = /^[a-z0-9.]+@[a-z0-9]+.[a-z]+(.[a-z]+)?$/i;
+  if (!emailPattern.test(email) || !await userModel.userEmailExists(email)) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'Incorrect email or password',
+    });
+  }
+
+  next();
+};
+
+const validateLoginPassword = async (req, res, next) => {
+  const { email, password = '' } = req.body;
+
+  if (password === '') {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'All fields must be filled',
+    });
+  }
+
+  const dbPassword = await userModel.getPasswordByEmail(email);
+  if (password !== dbPassword) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'Incorrect email or password',
+    });
+  }
+
+  next();
+};
+
 module.exports = {
   validateName,
   validateEmail,
   validatePassword,
+  validateLoginEmail,
+  validateLoginPassword,
 };
