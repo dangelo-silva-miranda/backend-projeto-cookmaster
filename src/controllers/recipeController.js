541bb1632c055d27fb97836e8f024cba79242af2
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-26 08:37:27 -0300

    refactor: :recycle: refactor createRecipe response

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -8,7 +8,7 @@ const createRecipe = async (req, res) => {
     { name, ingredients, preparation, user },
   );
 
-  return res.status(status).json({ ...message });
+  return res.status(status).json(message);
 };
 
 module.exports = {
