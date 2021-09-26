1b30c94a78d4d0c0aa642fb8d86b6e83203da408
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-26 15:28:15 -0300

    feat: :sparkles: implements userExists in recipeService

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -2,20 +2,29 @@ const { StatusCodes } = require('http-status-codes');
 const recipeModel = require('../models/recipeModel');
 const userModel = require('../models/userModel');
 
+const userExists = async (user) => {
+  const { id: userId, email, role } = user;
+  // Buscar o usuário no DB para obter seus dados atualizados    
+  const userDB = await userModel.getIdAndRoleByEmail(email);
+  
+  // Não existe usuário no DB com essas informações
+  if (!userDB || userId !== userDB.id || role !== userDB.role) {
+    return false;
+  }
+
+  return true;
+};
+
 /*
   Material consultado sobre como renomear chave durante desestruturação de objeto
   https://flaviocopes.com/how-to-rename-object-destructuring/
 */
 const createRecipe = async (
     { name, ingredients, preparation, user, image = '' }) => {
-  const { id: userId, email, role } = user;
+  const { id: userId } = user;
 
-  // Buscar o usuário no DB para obter seus dados atualizados    
-  const userDB = await userModel.getIdAndRoleByEmail(email);
-  
   // Não existe usuário no DB com essas informações
-  if (!userDB || userId !== userDB.id || role !== userDB.role) {
-    // console.log(`userId: ${typeof userId} | userDB.id: ${typeof userDB.id}`);
+  if (!userExists(user)) {
     return {
       status: StatusCodes.UNAUTHORIZED,
       message: { message: 'non-existent user.' },
