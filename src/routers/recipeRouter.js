6e2cf6af701f29734dce1ea1fa4335cce4daca39
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-30 13:13:52 -0300

    feat: :sparkles: adds recipeController.updateRecipeImageById in recipeRouter.get with address '/:id/image'

--- src/routers/recipeRouter.js
+++ src/routers/recipeRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
 const recipeController = require('../controllers/recipeController');
 
 const { validateName, validateIngredients, 
-  validatePreparation, validateJWT, validateAuth } = require('../middlewares/validations');
-
+  validatePreparation, validateJWT, 
+  validateAuth, isUserAuthorized, 
+  userExists } = require('../middlewares/validations');
+  
 const recipeRouter = express.Router();
 
 recipeRouter.post('/', 
@@ -12,6 +13,10 @@ recipeRouter.post('/',
   recipeController.createRecipe);
 
 recipeRouter.get('/', recipeController.getAllRecipes);
+
+recipeRouter.put('/:id/image', [validateAuth, validateJWT, 
+  userExists, isUserAuthorized],
+ recipeController.updateRecipeImageById);
 
 recipeRouter.get('/:id', recipeController.getRecipeById);
 
