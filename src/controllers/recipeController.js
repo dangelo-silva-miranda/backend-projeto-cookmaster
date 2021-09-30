760a78762eb4f4c01a014dabf2cf3f21c30fb48c
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-30 13:11:22 -0300

    feat: :sparkles: implements updateRecipeImageById in recipeController

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -1,3 +1,6 @@
+const { StatusCodes } = require('http-status-codes');
+const path = require('path');
+const { uploadSingle } = require('../middlewares/multer');
 const recipeService = require('../services/recipeService');
 
 const createRecipe = async (req, res) => {
@@ -51,10 +54,45 @@ const removeRecipeById = async (req, res) => {
   return res.status(status).send();
 };
 
+/*
+  Material consultado sobre checar tipo do arquivo antes realizar upload
+  https://medium.com/dataseries/configuring-express-multer-middleware-and-checking-file-information-497dc7af9eea
+  https://github.com/expressjs/multer/issues/336#issuecomment-242906859
+
+  Ver outras dicas:
+  https://github.com/expressjs/multer/issues/659
+*/
+const updateRecipeImageById = async (req, res) => {
+  uploadSingle(req, res, async (err) => {
+    if (err) {
+      const status = Number(err.message);
+      
+      if (status === StatusCodes.UNSUPPORTED_MEDIA_TYPE) {
+        return res.status(status).json({ message: 'unsupported media type' });
+      }
+
+      // An unknown error occurred when uploading.
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'unknown error when uploading' });
+    }
+    
+    const { file, params: { id } } = req;
+    
+    const imageRelativePath = file.path.slice(file.path.indexOf('src'));
+    const image = path.join('localhost:3000', imageRelativePath);
+    
+    const { status, message } = await recipeService.updateRecipeImageById(
+       { id, image },
+    );
+    
+    return res.status(status).json(message);
+  });
+};
+
 module.exports = {
   createRecipe,
   getAllRecipes,
   getRecipeById,
   updateRecipeById,
   removeRecipeById,
+  updateRecipeImageById,
 };
